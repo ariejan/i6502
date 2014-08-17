0e1d9eaa8f3c9dc78b3118d69cb4abf9a3deae2e
Ariejan de Vroom <ariejan@ariejan.net>
2014-08-17 16:20:30 +0200

    Add ROM Memory

--- ram_test.go
+++ ram_test.go
@@ -6,21 +6,18 @@ import (
 )
 
 func TestRamSize(t *testing.T) {
-	assert := assert.New(t)
-
 	ram, _ := NewRam(0x8000) // 32 kB
-	assert.Equal(0x8000, ram.Size())
+	assert.Equal(t, 0x8000, ram.Size())
 }
 
 func TestRamReadWrite(t *testing.T) {
-	assert := assert.New(t)
 	ram, _ := NewRam(0x8000) // 32 kB
 
 	// Ram zeroed out initially
 	for i := 0; i < 0x8000; i++ {
-		assert.Equal(0x00, ram.data[i])
+		assert.Equal(t, 0x00, ram.data[i])
 	}
 
 	ram.Write(0x1000, 0x42)
-	assert.Equal(0x42, ram.Read(0x1000))
+	assert.Equal(t, 0x42, ram.Read(0x1000))
 }
